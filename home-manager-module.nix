c35763d2beaca9abdd764b9894ad0d21d89af675
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-10-31 18:15:40 +0000

    fix: config merging

--- home-manager-module.nix
+++ home-manager-module.nix
@@ -57,48 +57,79 @@ in
     };
 
     settings = mkOption {
-      type = configFormat.type;
-      default = {
-        daemonize = {
-          log_to = "syslog";
-          pid_file = "/run/user/1000/illuminanced.pid";
-          log_level = "ERROR";
-        };
-        general = {
-          check_period_in_seconds = 1;
-          light_steps = 10;
-          min_backlight = 70;
-          step_barrier = 0.1;
-          max_backlight_file = "/sys/class/backlight/intel_backlight/max_brightness";
-          backlight_file = "/sys/class/backlight/intel_backlight/brightness";
-          illuminance_file = "/sys/bus/acpi/devices/ACPI0008:00/iio:device0/in_illuminance_raw";
-          event_device_mask = "/dev/input/event*";
-          event_device_name = "Asus WMI hotkeys";
-          enable_max_brightness_mode = true;
-          filename_for_sensor_activation = "";
-          switch_key_code = 560;
-        };
-        kalman = {
-          q = 1;
-          r = 20;
-          covariance = 10;
-        };
-        light = {
-          points_count = 6;
-          illuminance_0 = 0;
-          light_0 = 0;
-          illuminance_1 = 20;
-          light_1 = 1;
-          illuminance_2 = 300;
-          light_2 = 3;
-          illuminance_3 = 700;
-          light_3 = 4;
-          illuminance_4 = 1100;
-          light_4 = 5;
-          illuminance_5 = 7100;
-          light_5 = 10;
+      type = types.submodule {
+        freeformType = configFormat.type;
+        
+        options = {
+          daemonize = mkOption {
+            type = types.submodule {
+              freeformType = configFormat.type;
+              options = {};
+            };
+            default = {
+              log_to = "syslog";
+              pid_file = "/run/user/1000/illuminanced.pid";
+              log_level = "ERROR";
+            };
+          };
+          
+          general = mkOption {
+            type = types.submodule {
+              freeformType = configFormat.type;
+              options = {};
+            };
+            default = {
+              check_period_in_seconds = 1;
+              light_steps = 10;
+              min_backlight = 70;
+              step_barrier = 0.1;
+              max_backlight_file = "/sys/class/backlight/intel_backlight/max_brightness";
+              backlight_file = "/sys/class/backlight/intel_backlight/brightness";
+              illuminance_file = "/sys/bus/acpi/devices/ACPI0008:00/iio:device0/in_illuminance_raw";
+              event_device_mask = "/dev/input/event*";
+              event_device_name = "Asus WMI hotkeys";
+              enable_max_brightness_mode = true;
+              filename_for_sensor_activation = "";
+              switch_key_code = 560;
+            };
+          };
+          
+          kalman = mkOption {
+            type = types.submodule {
+              freeformType = configFormat.type;
+              options = {};
+            };
+            default = {
+              q = 1;
+              r = 20;
+              covariance = 10;
+            };
+          };
+          
+          light = mkOption {
+            type = types.submodule {
+              freeformType = configFormat.type;
+              options = {};
+            };
+            default = {
+              points_count = 6;
+              illuminance_0 = 0;
+              light_0 = 0;
+              illuminance_1 = 20;
+              light_1 = 1;
+              illuminance_2 = 300;
+              light_2 = 3;
+              illuminance_3 = 700;
+              light_3 = 4;
+              illuminance_4 = 1100;
+              light_4 = 5;
+              illuminance_5 = 7100;
+              light_5 = 10;
+            };
+          };
         };
       };
+      default = {};
       example = literalExpression ''
         {
           daemonize = {
